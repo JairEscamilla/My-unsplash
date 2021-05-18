34d586a2b5c1bb59899201f96a016665ebd93dce
Jair Escamilla <hernandez_yo14@outlook.es>
2021-05-17 19:41:50 -0500

    User Creation endpoint

--- api/routes/users.ts
+++ api/routes/users.ts
@@ -1,4 +1,6 @@
 import { Router, Express, Request, Response } from 'express';
+import { User } from '../models/user.model';
+import { response } from '../response';
 
 const usersApi = (app: Express) => {
   const router = Router();
@@ -7,6 +9,17 @@ const usersApi = (app: Express) => {
 
   router.get('', (req: Request, res: Response) => {
     res.status(200).send('Todo bien');
+  });
+
+  router.post('', async (req: Request, res: Response) => {
+    const { body } = req;
+    try{
+      const userCreated = await User.create(body);
+      response({ res: res, ok: true, status: 201, message: 'Usuario creado con éxito', extra_data: userCreated });
+    }catch(error){
+      console.error(`Ha ocurrido un error: ${error}`);
+      response({ res: res, ok: false, status: 500, message: 'Ha ocurrido un error al crear el usuario' });
+    }
   });
 }
 

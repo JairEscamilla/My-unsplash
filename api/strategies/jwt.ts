4bf6cea0d53dc6cbadf850468029f37527fda59a
Jair Escamilla <hernandez_yo14@outlook.es>
2021-05-18 20:59:44 -0500

    Adding JWT Strategy for authenticate requests

---
+++ api/strategies/jwt.ts
@@ -0,0 +1,24 @@
+import passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { config } from '../config/index';
+import { User } from '../models/user.model';
+
+
+const strategyConfig = {
+  secretOrKey: config.jwtKey,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+}
+
+passport.use(
+  new Strategy(strategyConfig, async (tokenPayload, cb) => {
+    try{
+      const user = await User.findOne({ email: tokenPayload.email });
+      if(!user)
+        return cb("No estas autorizado para acceder a esta ruta", user);
+      
+      cb(null, user);
+    }catch(error){
+      cb(error);
+    }
+  })
+);
